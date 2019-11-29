b2c0b518dc0796706b1193ea23cef19afd3db15f
sysrex <alex@sysrex.com>
2019-11-29 13:27:38 +0000

    prepare my snippets reviews system

---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.Handle("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	// Server static files
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sysrex/snippetreviews
 
 go 1.13
+
+require (
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+)
